fcb61c192039e016011afc49e923df93f0ee1799
kamga serges <serge.kamga@iforce5.com>
2023-12-19 08:03:31 +0100

    initialisation du projet argent bank

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, {FunctionComponent,useState, useEffect} from 'react';
+
+const App: FunctionComponent = () => {
+
+        //const name: String = 'React';
+
+       return ( 
+             <div>
+                <h1> Bonjour le monde !!!</h1>
+              </div>
+         );
+}
+
+export default App;
