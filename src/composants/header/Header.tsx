7901ae84ac6b1ac32b7186f3741e3327d258db6c
kamga serges <serge.kamga@iforce5.com>
2023-12-20 05:38:37 +0100

    creation du composant de login

--- src/composants/header/Header.tsx
+++ src/composants/header/Header.tsx
@@ -1,16 +1,31 @@
-import React, {FunctionComponent} from 'react';
+import React, { FunctionComponent } from 'react';
+import "../../styles/main.css";
+//import LogoFooter from '../';
 
 const Header: FunctionComponent = () => {
 
-        //const name: String = 'React';
+  //const name: String = 'React';
 
-       return ( 
-             <div>
-                <footer className="footer">
-                    <p className="footer-text">Copyright 2020 Argent Bank</p>
-                </footer>
-              </div>
-         );
+  return (
+    <div>
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="./index.html">
+          <img
+            className="main-nav-logo-image"
+            src="./../header/composants/assets/img/argentBankLogo.png"
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </a>
+        <div>
+          <a className="main-nav-item" href="./sign-in.html">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
---
+++ src/pages/sign-in/Sign-in.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from 'react';
+import "../../styles/main.css";
+//import LogoFooter from '../';
+
+const SignIn: FunctionComponent = () => {
+
+  //const name: String = 'React';
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default SignIn;
